253457ca96427e3d0e72259549bc48b64c5ef270
Muniba Yousuf <munibayousuf17@gmail.com>
2026-08-08 15:35:51 +0500

    For loop

--- muniba.py
+++ muniba.py
@@ -1,4 +1,4 @@
-i = 1 ; num=2
+"""i = 1 ; num=2
 while i<=10:
     print(num ," x " , i ," = " , num*i) 
     i=i+1
@@ -13,5 +13,18 @@ i=1
 while i<=5:
     print(i)
     i=i+1
-    if(i==3)
-        continue
+    if(i==3):
+        i=i+1
+        continue"""
+
+str = "Muniba"
+for char in str:
+    print(char)
+   
+idx =0
+x=98
+num = (2,87,98,56,87,98)
+for nums in num:
+    if(nums==x):
+        print("Number found at index", idx)
+    idx=idx+1
