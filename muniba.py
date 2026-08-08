c2330c1fb8fea98c3642dd6f14670e0a52f25bc3
Muniba Yousuf <munibayousuf17@gmail.com>
2026-08-08 15:17:36 +0500

    Continue statement

--- muniba.py
+++ muniba.py
@@ -1,4 +1,3 @@
-
 i = 1 ; num=2
 while i<=10:
     print(num ," x " , i ," = " , num*i) 
@@ -9,3 +8,10 @@ i=0
 while i< len(list):
     print(list[i])
     i=i+1
+
+i=1 
+while i<=5:
+    print(i)
+    i=i+1
+    if(i==3)
+        continue
