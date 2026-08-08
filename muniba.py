1a05d61183814bc5e4c5e8de0c413741824ee65e
Muniba Yousuf <munibayousuf17@gmail.com>
2026-08-08 19:48:08 +0500

    functions and reccursion

--- muniba.py
+++ muniba.py
@@ -38,10 +38,23 @@ for i in range (101,0,-1):
 n=5; sum=0
 for i in range(1, n+1):
     sum+=i
-print (sum)"""
+print (sum)
 
 n=5; fact=1
 for i in range(1, n+1):
     fact*=i
  
 print(fact)
+
+def sum(a,b):
+    s=a+b
+    print(s)
+    return s
+sum(2,3)"""
+
+def fact(n):
+    if(n==0 or n==1):
+        return 1
+    return (n*(fact(n-1)))
+
+print(fact(5))
